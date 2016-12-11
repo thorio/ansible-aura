9f762dc3bcb8f675d3a0681ba4efd16eef63d5e6
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2016-12-11 17:01:10 +1100

    Refactor methods into Aura class
    Since we were passing around the Ansible module and aura path to every
    method, it made sense to make a class to reduce the number of parameters
    needed in every function and to couple certain functions together.

--- aura.py
+++ aura.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 
+import itertools
+import os
+import re
+import shlex
+import sys
+
+from ansible.module_utils.basic import *
 
 # Aura module mangled from the existing pacman.py
 # This will only really deal with installation and upgrading; pacman can remove
@@ -9,9 +16,6 @@
 
 # TODO: Restructure this, and look at other modules for better examples
 # (apt, yum and homebrew are good examples).
-
-# TODO: Make aura class
-# We keep passing around aura_path and module when we don't need to.
 
 DOCUMENTATION = '''
 ---
@@ -68,16 +72,8 @@ EXAMPLES = '''
 
 '''
 
-import itertools
-import os
-import re
-import shlex
-import sys
 
 ANSI_ESCAPE_PATTERN = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
-
-# import module snippets
-from ansible.module_utils.basic import *
 
 def main():
     module = AnsibleModule(
@@ -90,7 +86,10 @@ def main():
         required_one_of = [['name', 'upgrade']],
         supports_check_mode = True)
 
-    aura_path = module.get_bin_path('aura', True)
+    
+    aura_path = module.get_bin_path('aura', required=True)
+
+    aura = Aura(module, aura_path)
 
     p = module.params
 
@@ -99,186 +98,175 @@ def main():
         p['state'] = 'present'
 
     if p['upgrade']:
-        upgrade(module, aura_path)
+        aura.upgrade()
 
     if p['name']:
         pkgs = p['name']
 
         if module.check_mode:
-            check_packages(module, aura_path, pkgs, p['state'])
+            aura.check_packages(pkgs, p['state'])
 
         if p['state'] in ['present', 'latest']:
-            install_packages(module, aura_path, p['state'], p['delmakedeps'], pkgs)
+            aura.install_packages(p['state'], p['delmakedeps'], pkgs)
 
+class Aura(object):
 
-def query_installation_info(module, aura_path, name):
-    """Queries the local installation.
+    def __init__(self, module, aura_path):
+        """
+        :param ansible.module_utils.basic.AnsibleModule: Ansible Module
+        :param str aura_path: Path to aura binary
+        """
+        self._module = module
+        self._aura_path = aura_path
+
+    def upgrade(self):
+        upgrade_command = "%s -A --sysupgrade --noconfirm" % self._aura_path
+        upgrade_command_dry = "%s -A --sysupgrade --dryrun" % self._aura_path
+        rc, stdout, stderr = self._module.run_command(upgrade_command_dry,
+                                                      check_rc=False)
     
-    :param module: Ansible Module
-    :param str aura_path:
-    :param str name: Package name
-    :rtype: dict[str, str]
-    """
-    query_command = "%s --query --info %s" % (aura_path, name)
-    rc, stdout, _ = module.run_command(query_command, check_rc=False)
-    if rc == 0:
-        return extract_info(stdout)
-    return {}
-    
-def query_aura_info(module, aura_path, name):
-    """Queries the AUR for the package.
-    Returns an empty dict if package could not be found.
-    
-    :param module: Ansible Module
-    :param str aura_path:
-    :param str name: Package name
-    :rtype: dict[str, str]
-    """
-    query_command = "%s --aursync --info %s" % (aura_path, name)
-    _, stdout, _ = module.run_command(query_command, check_rc=False)
-
-    if stdout.strip():
-        return extract_info(stdout)
-    return {}
-
-def needs_installation(module, aura_path, name, state):
-    '''Determines whether we need to install the package
-    :param ansible.module_utils.basic.AnsibleModule: Ansible Module
-    :param str aura_path:
-    :param str name:
-    :param str state:
-    ''' 
-
-    # First check that the package exists on the AUR
-    aur_info = query_aura_info(module, aura_path, name)
-    if not aur_info:
-        module.fail_json(msg="No package '%s' found on AUR." % name)
-
-    local_info = query_installation_info(module, aura_path, name)
-    if not local_info:
-        # If the package does not exist locally, then it needs installing
-        # regardless of state
-        return True
-
-    local_version = local_info['Version']
-    print(local_version)
-    aur_version = aur_info['Version']
-    print(aur_version)
-
-    # if state is latest, check that the versions match, otherwise 
-    # if state is not latest (must be present), then the package exists but we
-    # don't need to upgrade it.
-    return state == 'latest' and local_version != aur_version
-
-
-def upgrade(module, aura_path):
-    '''
-    :param ansible.module_utils.basic.AnsibleModule module: Ansible module
-    :param str aura_path: Path to aura binary
-    '''
-
-    upgrade_command = "%s -A --sysupgrade --noconfirm" % (aura_path)
-    upgrade_command_dry = "%s -A --sysupgrade --dryrun" % (aura_path)
-    rc, stdout, stderr = module.run_command(upgrade_command_dry,
-                                            check_rc=False)
-
-    # So: 0 return on packages to upgrade, 1 if there is nothing to upgrade
-    if rc == 0:
-        if module.check_mode:
-            data = stdout.split('\n')
-            packages = list(itertools.takewhile(
-                lambda line: line != '',
-                itertools.dropwhile(
-                    lambda line: 'aura >>=' in line,
-                    data)))
-            module.exit_json(
-                changed=True,
-                msg="%s package(s) would be upgraded" % (len(packages)))
-        rc, stdout, stderr = module.run_command(upgrade_command, check_rc=False)
+        # So: 0 return on packages to upgrade, 1 if there is nothing to upgrade
         if rc == 0:
-            module.exit_json(changed=True, msg='System upgraded')
+            if self._module.check_mode:
+                data = stdout.split('\n')
+                packages = list(itertools.takewhile(
+                    lambda line: line != '',
+                    itertools.dropwhile(
+                        lambda line: 'aura >>=' in line,
+                        data)))
+                self._module.exit_json(
+                    changed=True,
+                    msg="%s package(s) would be upgraded" % len(packages))
+            rc, stdout, stderr = self._module.run_command(upgrade_command,
+                                                          check_rc=False)
+            if rc == 0:
+                self._module.exit_json(changed=True, msg='System upgraded')
+            else:
+                self._module.fail_json(changed=False, msg="Could not upgrade")
         else:
-            module.fail_json(msg="Could not upgrade")
-    else:
-        module.exit_json(changed=False, msg="No AUR packages to upgrade")
+            self._module.exit_json(changed=False,
+                                   msg="No AUR packages to upgrade")
 
 
-def install_packages(module, aura_path, state, delmakedeps, packages):
-    '''
-    :type state: str
-    :type delmakedeps: bool
-    :type packages: list[str]
-    '''
-    successful_installs = 0
-    packages_to_install = (package
+    def install_packages(self, state, delmakedeps, packages):
+        '''
+        :type state: str
+        :type delmakedeps: bool
+        :type packages: list[str]
+        '''
+        successful_installs = 0
+        packages_to_install = (package
+                               for package in packages
+                               if self._needs_installation(package, state))
+        for package in packages_to_install:
+            params = '--aursync %s' % package
+            
+            if delmakedeps:
+                params += ' --delmakedeps'
+    
+            command = "%s %s --noconfirm" % (self._aura_path, params)
+            rc, stdout, stderr = self._module.run_command(command,
+                                                          check_rc=False)
+    
+            if rc != 0:
+                self._module.fail_json(
+                    msg="Failed to install package '%s'." % package)
+    
+            successful_installs += 1
+    
+        if successful_installs > 0:
+            self._module.exit_json(
+                changed=True,
+                msg="Installed %s package(s)." % successful_installs)
+    
+        self._module.exit_json(changed=False,
+                               msg="All packages already installed.")
+
+
+    def check_packages(self, packages, state):
+        num_changed = len([package
                            for package in packages
-                           if needs_installation(module,
-                                                 aura_path,
-                                                 package,
-                                                 state))
-    for package in packages_to_install:
-        params = '--aursync %s' % package
+                           if self._needs_installation(package, state)])
+        if num_changed:
+            self._module.exit_json(
+                changed=True,
+                msg="%s packages would be changed to %s" % (num_changed,
+                                                            state))
+        else:
+            self._module.exit_json(
+                changed=False,
+                msg='%s packages are already %s' % (num_changed, state))
+
+    def _needs_installation(self, name, state):
+        """Determines whether we need to install the package
+
+        :param str name: Package name.
+        :param str state: Desired state of package.
+        :rtype: bool
+        """
+
+        # First check that the package exists on the AUR
+        aur_info = self._query_aura_info(name)
+        if not aur_info:
+            self._module.fail_json(msg="No package '%s' found on AUR." % name)
+
+        local_info = self._query_installation_info(name)
+        if not local_info:
+            # If the package does not exist locally, then it needs installing
+            # regardless of state
+            return True
+
+        local_version = local_info['Version']
+        aur_version = aur_info['Version']
+ 
+        # if state is latest, check that the versions match, otherwise
+        # if state is not latest (must be present), then the package exists
+        # but we don't need to upgrade it.
+        return state == 'latest' and local_version != aur_version
+
+    def _query_installation_info(self, name):
+        """Queries the local installation.
+
+        :param str name: Package name
+        :rtype: dict[str, str]
+        """
+        query_command = "%s --query --info %s" % (self._aura_path, name)
+        rc, stdout, _ = self._module.run_command(query_command, check_rc=False)
+        if rc == 0:
+            return self._extract_info(stdout)
+        return {}
+
+    def _query_aura_info(self, name):
+        """Queries the AUR for the package.
+        Returns an empty dict if package could not be found.
         
-        if delmakedeps:
-            params += ' --delmakedeps'
+        :param str name: Package name.
+        :rtype: dict[str, str]
+        """
+        query_command = "%s --aursync --info %s" % (self._aura_path, name)
+        _, stdout, _ = self._module.run_command(query_command, check_rc=False)
+    
+        if stdout.strip():
+            return self._extract_info(stdout)
+        return {}
 
-        # TODO: --needed will not install it if already present... Could use this?
-        # If state == present, plug in '--needed', and record number of packges
-        # that don't output 'The following packages are already installed'.
-        # Kind of doing too much in one step, though.
-        command = "%s %s --noconfirm" % (aura_path, params)
-        rc, stdout, stderr = module.run_command(command, check_rc=False)
-
-        if rc != 0:
-            module.fail_json(msg="Failed to install package '%s'." % package)
-
-        successful_installs += 1
-
-    if successful_installs > 0:
-        module.exit_json(changed=True, msg="Installed %s package(s)." % successful_installs)
-
-    module.exit_json(changed=False, msg="All packages already installed.")
-
-
-def check_packages(module, aura_path, packages, state):
-    num_changed = len([package
-                       for package in packages
-                       if needs_installation(module,
-                                             aura_path,
-                                             package,
-                                             state)])
-    if num_changed:
-        module.exit_json(
-            changed=True,
-            msg="%s packages would be changed to %s" % (num_changed, state))
-    else:
-        module.exit_json(
-            changed=False,
-            msg='%s packages are already %s' % (num_changed, state))
-
-def get_version(output):
-    """
-    :type output: str 
-    :rtype: str
-    """
-    return extract_info(output).get('Version')
-
-def extract_info(info):
-    """Retrieves values from either pacman or aura output and returns a
-    dictionary equivalent.
-
-    :param str info: the output from either 'pacman -Ai' or 'aura -Ai'
-    :returns: A dictionary containing the information needed.
-    :rtype: dict[str, str]
-    """
-    colourless_info = re.sub(ANSI_ESCAPE_PATTERN, '', info)
-    lines = colourless_info.split('\n')
-    info_dict = {}
-    for line in lines:
-        if line:
-            key, value = line.split(':', 1)
-            info_dict[key.strip()] = value.strip()
-    return info_dict
+    @staticmethod
+    def _extract_info(info):
+        """Retrieves values from either pacman or aura output and returns a
+        dictionary equivalent.
+    
+        :param str info: the output from either 'pacman -Ai' or 'aura -Ai'
+        :returns: A dictionary containing the information needed.
+        :rtype: dict[str, str]
+        """
+        colourless_info = re.sub(ANSI_ESCAPE_PATTERN, '', info)
+        lines = colourless_info.split('\n')
+        info_dict = {}
+        for line in lines:
+            if line:
+                key, value = line.split(':', 1)
+                info_dict[key.strip()] = value.strip()
+        return info_dict
 
 
 
