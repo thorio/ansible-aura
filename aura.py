5dd70b8f76fc2794e1750b1ac5a7825f306a488a
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2018-01-29 15:31:23 +1100

    Expose 'build' and 'builduser' options
    This way we can actually build as a certain user (even if that user
    doesn't have sudo rights).
    
    References #1

--- aura.py
+++ aura.py
@@ -41,6 +41,19 @@ options:
         choices: ['yes', 'no']
         aliases: ['sysupgrade']
 
+    build:
+        description:
+            - Path to use when building AUR packages. By default the cache
+              directory configured in /etc/pacman.conf is used.
+        required: false
+        aliases: ['buildpath']
+
+    builduser:
+        description:
+            - User to build packages as.
+        required: false
+        default: 'nobody'
+
     delmakedeps:
         description:
             - Whether or not to uninstall build dependencies that are no longer
@@ -70,6 +83,8 @@ def main():
             name        = dict(aliases=['pkg', 'package'], type='list'),
             state       = dict(default='present', choices=['present', 'installed', 'latest']),
             upgrade     = dict(aliases=['sysupgrade'], default=False, type='bool'),
+            buildpath   = dict(aliases=['build', 'buildpath'], type='path'),
+            builduser   = dict(aliases=['builduser'], default='nobody'),
             delmakedeps = dict(default=False, type='bool')),
         required_one_of=[['name', 'upgrade']],
         supports_check_mode=True)
@@ -87,7 +102,8 @@ def main():
     if params['upgrade']:
         if module.check_mode:
             aura.check_upgrade()
-        aura.upgrade()
+        aura.upgrade(buildpath=params['buildpath'],
+                     builduser=params['builduser'])
 
     if params['name']:
         packages = params['name']
@@ -96,9 +112,11 @@ def main():
             aura.check_packages(packages, params['state'])
 
         if params['state'] in ['present', 'latest']:
-            aura.install_packages(packages,
-                                  params['state'],
-                                  params['delmakedeps'])
+            aura.install_packages(packages=packages,
+                                  state=params['state'],
+                                  buildpath=params['buildpath'],
+                                  builduser=params['builduser'],
+                                  delmakedeps=params['delmakedeps'])
 
 class Aura(object):
     """A class used to execute Aura commands."""
@@ -113,11 +131,17 @@ class Aura(object):
         self._aura_path = aura_path
 
 
-    def upgrade(self):
-        """Upgrade all AUR packages on the system."""
+    def upgrade(self, buildpath, builduser):
+        """
+        Upgrade all AUR packages on the system.
+        :type buildpath: Optional[str]
+        :type builduser: str
+        """
         packages_to_upgrade = self._packages_to_upgrade()
 
-        upgrade_command = "%s -A --sysupgrade --noconfirm" % self._aura_path
+        upgrade_command = "%s --aursync --builduser=%s --sysupgrade --noconfirm" % (self._aura_path, builduser)
+        if buildpath is not None:
+            upgrade_command += " --buildpath=%s" % buildpath
         rc, _, _ = self._module.run_command(upgrade_command, check_rc=False)
         if rc == 0:
             self._module.exit_json(
@@ -160,10 +184,14 @@ class Aura(object):
         return packages
 
 
-    def install_packages(self, packages, state, delmakedeps):
+    def install_packages(self, packages, state,
+                         buildpath, builduser,
+                         delmakedeps):
         """
         :type packages: list[str]
         :type state: str
+        :type buildpath: Optional[str]
+        :type builduser: str
         :type delmakedeps: bool
         """
         successful_installs = 0
@@ -171,7 +199,10 @@ class Aura(object):
                                for package in packages
                                if self._needs_installation(package, state))
         for package in packages_to_install:
-            params = "--aursync --builduser=nobody %s" % package
+            params = "--aursync --builduser=%s %s" % (builduser, package)
+
+            if buildpath is not None:
+                params += " --build=%s" % buildpath
 
             if delmakedeps:
                 params += " --delmakedeps"
